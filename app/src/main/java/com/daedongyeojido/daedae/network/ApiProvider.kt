7a276fc75bb301278332e26a8b617189bcece761
hseun <hseun0732@dsm.hs.kr>
2024-04-02 10:45:49 +0900

    del :: baseUrl

--- app/src/main/java/com/daedongyeojido/daedae/network/ApiProvider.kt
+++ app/src/main/java/com/daedongyeojido/daedae/network/ApiProvider.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiProvider {
-    private val BASE_URL = "https://prod-server.xquare.app/jung-ho/"
+    private val BASE_URL = ""
 
     private val instance: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
